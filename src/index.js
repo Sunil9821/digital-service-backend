956289f29a1769a1b2427dc496f6d3b4db07e8dc
Sunil9821 <mauryasunil1305@gmail.com>
2025-08-10 12:11:43 +0530

    first-commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const port = process.env.PORT || 3200;
+const router = require('./route');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors(
+    {origin: ['http://localhost:4200']}
+))
+app.use('', router);
+
+
+
+app.listen(port, ()=>{
+    console.log('listening on port '+port);
+})
